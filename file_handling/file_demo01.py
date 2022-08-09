9f948fbdc558d721ce47cfd8c9d0d58f11d31438
soongonkim <soongon@hucloud.co.kr>
2022-08-09 16:34:58 +0900

    file demo

--- file_handling/file_demo01.py
+++ file_handling/file_demo01.py
@@ -1,4 +1,9 @@
-test_file = open('test.txt')
-lines = test_file.readlines()
-print(lines)
-test_file.close()
+# test_file = open('test.txt')
+# lines = test_file.readlines()
+# print(lines)
+# test_file.close()
+
+
+with open('test.txt') as test_file:
+    lines = test_file.readlines()
+    print(lines)
--- file_handling/file_write_demo02.py
+++ file_handling/file_write_demo02.py
@@ -1,5 +1,12 @@
-handler = open('new-file.txt', 'w')
-handler.write('hello world\n')
-handler.write('좋습니다. 아마존..\n')
-handler.close()
+# handler = open('new-file.txt', 'w')
+# handler.write('hello world\n')
+# handler.write('좋습니다. 아마존..\n')
+# handler.close()
+# print('작성 완료')
+
+
+with open('new-file.txt', 'w') as handler:
+    handler.write('hello world\n')
+    handler.write('좋습니다. 아마존..\n')
+
 print('작성 완료')
